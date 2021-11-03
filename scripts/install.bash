58df3f95ca6084fcbe969324210351a534e7dab8
ya0201 <you@example.com>
2021-11-03 22:13:37 +0900

    reload daemon in install.sh

--- scripts/install.bash
+++ scripts/install.bash
@@ -2,9 +2,12 @@
 
 mkdir -p ${HOME}/.config/systemd/user
 cp ./services/yukarin-voice-caster* ${HOME}/.config/systemd/user
+
 for f in $(find services -name *.service); do
   systemctl --user enable $(basename $f)
 done
 for f in $(find services -name *.timer); do
   systemctl --user enable $(basename $f)
 done
+
+systemctl --user daemon-reload
